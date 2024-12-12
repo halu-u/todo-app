6bb5a64ea8e5b267e2f0125cc88fc371389cea73
kalin <nakajima.ctf@gmail.com>
2024-12-12 20:07:21 +0900

    Add feature: Ability to add todos

--- todo.py
+++ todo.py
@@ -1,9 +1,33 @@
+todos = []
+
+def add_todo():
+    todo = input("Enter a new Todo: ")
+    todos.append(todo)
+    print(f"'{todo}' has been added!")
+
 def main():
-    print("Todo App")
-    print("1. Add Todo")
-    print("2. View Todos")
-    print("3. Delete Todo")
-    print("4. Exit")
+    while True:
+        print("Todo App")
+        print("1. Add Todo")
+        print("2. View Todos")
+        print("3. Delete Todo")
+        print("4. Exit")
+        choice = input("choose an option: ")
+
+        if choice == "1":
+            add_todo()
+        elif choice == "2":
+            print("\n".join(todos) if todos else "No todos yet!")
+
+        elif choice == "3":
+            print("Feature not implemented yet.")
+
+        elif choice == "4":
+            print("Goodbye!")
+            break
+
+        else:
+            print("Invalid choice. Please try again.")
 
 if __name__ =="__main__":
     main()
